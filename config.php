329c2aa42056747f0705b1eebedd52d246e4b23a
ReemAlja <124820193+ReemAlja@users.noreply.github.com>
2023-04-27 23:01:01 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "offices";
+    
+  // Data Source Network
+  $dsn = 'mysql:host=' . $servername . ';dbname=' . $dbname . '';
+
+  // Connection Variable
+  $conn = null;
+
+  // Connect Using PDO (PHP Data Output)
+  try {
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  } catch (PDOException $e) {
+    die('Error : ' . $e->getMessage());
+  }
+?>
